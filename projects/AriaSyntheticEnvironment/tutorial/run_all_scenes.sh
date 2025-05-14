583b0c4bea71d85965d63c3b56376a30c9ec91dc
Sahithya Ravi <sahithyaravi@Sahithyas-Air.hitronhub.home>
2025-05-14 13:37:21 -0700

    gpt4o

--- projects/AriaSyntheticEnvironment/tutorial/run_all_scenes.sh
+++ projects/AriaSyntheticEnvironment/tutorial/run_all_scenes.sh
@@ -17,3 +17,24 @@ for scene_dir in "$DATASET_PATH"/*/; do
     python bev_per_scene.py --dataset_path "$DATASET_PATH" --scene_id "$scene_id" --output_folder "$OUTPUT_FOLDER"
     echo "Finished processing scene $scene_id."
 done
+
+
+# REPO_DIR="/data/refactored-carnival"
+# cd $REPO_DIR
+
+# source ~/anaconda3/etc/profile.d/conda.sh
+# conda activate refactored-carnival
+
+
+# log="logs/run_gpt4o_3d_topdown.log"
+# out="output_3d/gpt4o/gpt4o_3d_topdown.csv"
+# bird_eye_folder=""
+
+# python main_gpt4o.py \
+#     --file_path ./spatial_reasoning_qa_val_natural.json \
+#     --save_path "$out" \
+#     --use_bird_eye_scene_view \
+#     --bird_eye_path $bird_eye_folder \
+#     --mark_bbox \
+#     --sample_size 600\
+#   2>&1 | tee logs/gpt4o_topdown.log
